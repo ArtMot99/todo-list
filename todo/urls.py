f774bd6ec02974c0d336adc58e3936039f8c3c89
Artem Motrunich <motrunich2013@gmail.com>
2023-06-07 20:01:52 +0200

    Add path for delete task

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskListView, task_complete, task_undo, TaskCreateView, TaskUpdateView
+from .views import TaskListView, task_complete, task_undo, TaskCreateView, TaskUpdateView, TaskDeleteView
 
 
 app_name = "todo"
@@ -8,6 +8,7 @@ urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("task/create/", TaskCreateView.as_view(), name="task-create"),
     path("task/<int:task_id>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("task/<int:task_id>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("complete_task/<int:task_id>/", task_complete, name="complete-task"),
     path("undo_task/<int:task_id>/", task_undo, name="undo-task"),
 ]
