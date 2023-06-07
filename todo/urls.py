a9e37dca19bbc29487cb98954046bdfe38e1780e
Artem Motrunich <motrunich2013@gmail.com>
2023-06-07 19:41:17 +0200

    Add path for create task

--- todo/urls.py
+++ todo/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import TaskListView, task_complete, task_undo
+from .views import TaskListView, task_complete, task_undo, TaskCreateView
 
 
 app_name = "todo"
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
+    path("task/create/", TaskCreateView.as_view(), name="task-create"),
     path("complete_task/<int:task_id>/", task_complete, name="complete-task"),
     path("undo_task/<int:task_id>/", task_undo, name="undo-task"),
 ]
