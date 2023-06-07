861d6fe7ac5990f3f27e48106ddabe5a84d19a73
Artem Motrunich <motrunich2013@gmail.com>
2023-06-07 19:14:28 +0200

    Add paths for undo & complete button

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import TaskListView
+from .views import TaskListView, task_complete, task_undo
 
 
 app_name = "todo"
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="task-list")
+    path("", TaskListView.as_view(), name="task-list"),
+    path("complete_task/<int:task_id>/", task_complete, name="complete-task"),
+    path("undo_task/<int:task_id>/", task_undo, name="undo-task"),
 ]
