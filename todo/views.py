8c372266f6c6dc21faaca6ddff01b95742c8b334
Artem Motrunich <motrunich2013@gmail.com>
2023-06-07 19:16:59 +0200

    Add TaskListView & 2 views for complete | undo button

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import ListView
 
-# Create your views here.
+from todo.models import Task
+
+
+class TaskListView(ListView):
+    model = Task
+    paginate_by = 7
+
+
+def task_complete(request, task_id) -> HttpResponse:
+    task = get_object_or_404(Task, id=task_id)
+    task.is_done = True
+    task.save()
+    return redirect("todo:task-list")
+
+
+def task_undo(request, task_id) -> HttpResponse:
+    task = get_object_or_404(Task, id=task_id)
+    task.is_done = False
+    task.save()
+    return redirect("todo:task-list")
