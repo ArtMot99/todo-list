20c578828e05c4b1da1beadae62c70d8f944833a
Artem Motrunich <motrunich2013@gmail.com>
2023-06-07 19:52:59 +0200

    Add TaskUpdateView to views.py

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 
 from todo.forms import TaskForm
 from todo.models import Task
@@ -17,6 +17,18 @@ class TaskCreateView(CreateView):
     form_class = TaskForm
     template_name = "todo/task_form.html"
     success_url = reverse_lazy("todo:task-list")
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "todo/task_form.html"
+    success_url = reverse_lazy("todo:task-list")
+
+    def get_object(self, queryset=None):
+        task_id = self.kwargs.get("task_id")
+        return get_object_or_404(Task, id=task_id)
+
 
 
 def task_complete(request, task_id) -> HttpResponse:
