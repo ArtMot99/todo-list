8e07220468b137c8da4691e019431a26c4ceea66
Artem Motrunich <motrunich2013@gmail.com>
2023-06-08 21:03:22 +0200

    Add all views for tag CRUD

--- todo/views.py
+++ todo/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from todo.forms import TaskForm
-from todo.models import Task
+from todo.models import Task, Tag
 
 
 class TaskListView(ListView):
@@ -15,14 +15,12 @@ class TaskListView(ListView):
 class TaskCreateView(CreateView):
     model = Task
     form_class = TaskForm
-    template_name = "todo/task_form.html"
     success_url = reverse_lazy("todo:task-list")
 
 
 class TaskUpdateView(UpdateView):
     model = Task
     form_class = TaskForm
-    template_name = "todo/task_form.html"
     success_url = reverse_lazy("todo:task-list")
 
     def get_object(self, queryset=None):
@@ -38,6 +36,37 @@ class TaskDeleteView(DeleteView):
     def get_object(self, queryset=None):
         task_id = self.kwargs.get("task_id")
         return get_object_or_404(Task, id=task_id)
+
+
+class TagListView(ListView):
+    model = Tag
+    paginate_by = 7
+
+
+class TagCreateView(CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagUpdateView(UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+
+    def get_object(self, queryset=None):
+        tag_id = self.kwargs.get("tag_id")
+        return get_object_or_404(Tag, id=tag_id)
+
+
+class TagDeleteView(DeleteView):
+    model = Tag
+    template_name = "todo/tag_confirm_delete.html"
+    success_url = reverse_lazy("todo:tag-list")
+
+    def get_object(self, queryset=None):
+        tag_id = self.kwargs.get("tag_id")
+        return get_object_or_404(Tag, id=tag_id)
 
 
 def task_complete(request, task_id) -> HttpResponse:
