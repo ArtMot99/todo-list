afe0ce243b191adb20f6bbceb081cffa62bbe1d0
Artem Motrunich <motrunich2013@gmail.com>
2023-06-08 21:01:41 +0200

    Add paths for all tags pages CRUD

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path
-from .views import TaskListView, task_complete, task_undo, TaskCreateView, TaskUpdateView, TaskDeleteView
+from .views import (
+    TaskListView,
+    task_complete,
+    task_undo,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+)
 
 
 app_name = "todo"
@@ -11,4 +22,9 @@ urlpatterns = [
     path("task/<int:task_id>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("complete_task/<int:task_id>/", task_complete, name="complete-task"),
     path("undo_task/<int:task_id>/", task_undo, name="undo-task"),
+
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tag/<int:tag_id>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tag/<int:tag_id>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
